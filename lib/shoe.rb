24697f47e0fe5400bceba054092f494b0e1268f5
user <github email address>
2020-11-07 13:02:08 -0500

    commit tree 3c754c315c69413965929548710ef38db3efdbe9

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
-# Make your shoe class here!
+# Make your shoe class here!\
+require 'pry'
+class Shoe
+    
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand  
+
+    def initialize(brand)
+        @brand = brand
+    end 
+
+    # def condition=(shape)
+    #     @condition = shape
+    #     # binding.pry 
+    # end 
+
+    # def condition
+    #     @condition
+    # end 
+
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+
+
+     end 
+   
+
+
+end 
